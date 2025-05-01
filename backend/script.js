2021070c2ec3a09b8ac5a50431496ccd6ede14de
AndriYTC <andriyanto28021990@gmail.com>
2025-05-01 09:23:47 +0700

    first commit

---
+++ backend/script.js
@@ -0,0 +1,16 @@
+const ContactForms = document.getElementById("contact-form");
+
+ContactForms.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+    const url = event.target.action; // Get the form action URL
+    const formData = new FormData(ContactForms); // Create a FormData object from the form
+
+    fetch(url, {
+        method: "POST",
+        body: formData,
+        mode: "no-cors",
+    }).then (() => {
+        window.location.href = "thankyou.html"; // Redirect to thank you page on success
+    }).catch((error) => alert("Error: " + error)); // Handle any errors that occur during the fetch
+});
