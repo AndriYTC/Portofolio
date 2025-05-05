e7651e12e2d6e8ee5b3c3ef49c441a0ee7fe3be8
AndriYTC <andriyanto28021990@gmail.com>
2025-05-05 20:24:18 +0700

    update

--- asset/backend/script.js
+++ asset/backend/script.js
@@ -3,14 +3,31 @@ const ContactForms = document.getElementById("contact-form");
 ContactForms.addEventListener("submit", function (event) {
   event.preventDefault(); // Prevent the default form submission behavior
 
-    const url = event.target.action; // Get the form action URL
-    const formData = new FormData(ContactForms); // Create a FormData object from the form
+  const url = event.target.action; // Get the form action URL
+  const formData = new FormData(ContactForms); // Create a FormData object from the form
 
-    fetch(url, {
-        method: "POST",
-        body: formData,
-        mode: "no-cors",
-    }).then (() => {
-        window.location.href = "thankyou.html"; // Redirect to thank you page on success
-    }).catch((error) => alert("Error: " + error)); // Handle any errors that occur during the fetch
+  fetch(url, {
+    method: "POST",
+    body: formData,
+    mode: "no-cors",
+  })
+    .then(() => {
+      window.location.href = "thankyou.html"; // Redirect to thank you page on success
+    })
+    .catch((error) => alert("Error: " + error)); // Handle any errors that occur during the fetch
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed("#typed-text", {
+    strings: [
+      "Andri.",
+      "Junior Web Programmer.",
+    ],
+    typeSpeed: 80,
+    backSpeed: 90,
+    backDelay: 2000,
+    loop: true,
+    showCursor: true,
+    cursorChar: "_",
+  });
 });
